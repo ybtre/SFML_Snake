63ae4734315464542a9d8f8e2b2bcae30f54ef71
Ricci Vuchev <vuchev7892@gmail.com>
2020-02-27 23:41:44 +0000

    messing around with a bouncing mushroom

--- Snake/Timber.cpp
+++ Snake/Timber.cpp
@@ -3,19 +3,41 @@
 using namespace sf;
 
 void main(int argc, char** argv[]) {
-	RenderWindow window(VideoMode(640, 480), "Window");
+	sf::RenderWindow window(sf::VideoMode(640, 480),
+		"Rendering the rectangle.");
+
+	sf::Texture mushroomTexture;
+	mushroomTexture.loadFromFile("Mushroom.png");
+
+	sf::Sprite mushroom(mushroomTexture);
+
+	sf::Vector2u size = mushroomTexture.getSize();
+	mushroom.setOrigin(size.x / 2, size.y / 2);
+	sf::Vector2f increment(0.4f, 0.4f);
 
 	while (window.isOpen()) {
-		Event event;
+		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed) {
+			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 		}
+		if ((mushroom.getPosition().x + (size.x / 2) > window.getSize().x&& increment.x > 0)
+			|| (mushroom.getPosition().x - (size.x / 2) < 0
+				&& increment.x < 0)) {
+			// Reverse the direction on X axis.
+			increment.x = -increment.x;
+		}
+		if ((mushroom.getPosition().y + (size.y / 2) > window.getSize().y&& increment.y > 0)
+			|| (mushroom.getPosition().y - (size.y / 2) < 0
+				&& increment.y < 0)) {
+			// Reverse the direction on Y axis.
+			increment.y = -increment.y;
+		}
 
-		window.clear(Color::Black);
-
-		// draw
+		mushroom.setPosition(mushroom.getPosition() + increment);
+		window.clear(sf::Color(16, 16, 16, 255)); // Dark gray.
+		window.draw(mushroom); // Drawing our sprite.
 		window.display();
 	}
 }
