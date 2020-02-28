162177e9131347216c4f7de3151c292213217116
Ricci Vuchev <vuchev7892@gmail.com>
2020-02-28 23:29:11 +0000

    most of the snake game implemented and finished

---
+++ Snake/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Window.h"
+#include "World.h"
+#include "Snake.h"
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void HandleInput();
+	void Update();
+	void Render();
+
+	sf::Time GetElapsed();
+	void RestartClock();
+
+	Window* GetWindow();
+private:
+	Window m_window;
+	sf::Clock m_clock;
+	float m_elapsed;
+
+	World m_world;
+	Snake m_snake;
+};
